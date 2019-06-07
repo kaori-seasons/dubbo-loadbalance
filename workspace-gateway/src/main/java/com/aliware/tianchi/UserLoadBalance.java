55f4242d26d58424e0f80e197a91b60d8511b4d7
DESKTOP-6QTD8HA\complome <yuluoxinsheng@gmail.com>
2019-06-07 20:41:10 +0800

    基于一致性hash负载均衡的实现

---
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -0,0 +1,123 @@
+package com.aliware.tianchi;
+
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.RpcException;
+import org.apache.dubbo.rpc.cluster.LoadBalance;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author daofeng.xjf
+ *
+ * 负载均衡扩展接口
+ * 必选接口，核心接口
+ * 此类可以修改实现，不可以移动类或者修改包名
+ * 选手需要基于此类实现自己的负载均衡算法
+ */
+public class UserLoadBalance implements LoadBalance {
+
+    @Override
+    public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
+
+        String methodName = RpcUtils.getMethodName(invocation);
+        String key = invokers.get(0).getUrl().getServiceKey() + "." + methodName;
+        int identityHashCode = System.identityHashCode(invokers);
+        UniformityHashSelector<T> selector = (UniformityHashSelector<T>) selectors.get(key);
+        if (selector == null || selector.identityHashCode != identityHashCode) {
+            selectors.put(key, new UniformityHashSelector<T>(invokers, methodName, identityHashCode));
+            selector = (UniformityHashSelector<T>) selectors.get(key);
+        }
+        return selector.select(invocation);
+
+
+        return invokers.get(ThreadLocalRandom.current().nextInt(invokers.size()));
+    }
+
+
+    private static final class UniformityHashSelector<T> {
+
+        private final TreeMap<String, Invoker<T>> virtualInvokers;
+
+        private final int replicaNumber;
+
+        private final int identityHashCode;
+
+        private final int[] argumentIndex;
+
+        UniformityHashSelector(List<Invoker<T>> invokers, String methodName, int identityHashCode) {
+            this.virtualInvokers = new TreeMap<Long, Invoker<T>>();
+            this.identityHashCode = identityHashCode;
+            URL url = invokers.get(0).getUrl();
+            this.replicaNumber = url.getMethodParameter(methodName, HASH_NODES, 160);
+            String[] index = Constants.COMMA_SPLIT_PATTERN.split(url.getMethodParameter(methodName, HASH_ARGUMENTS, "0"));
+            argumentIndex = new int[index.length];
+            for (int i = 0; i < index.length; i++) {
+                argumentIndex[i] = Integer.parseInt(index[i]);
+            }
+            for (Invoker<T> invoker : invokers) {
+                String address = invoker.getUrl().getAddress();
+                for (int i = 0; i < replicaNumber / 5; i++) {
+                    byte[] digest = md5(address + i);
+                    for (int h = 0; h < 4; h++) {
+                        String m =byteToString(digest, h);
+                        virtualInvokers.put(m, invoker);
+                    }
+                }
+            }
+        }
+
+        public Invoker<T> select(Invocation invocation) {
+            String key = toKey(invocation.getArguments());
+            byte[] digest = md5(key);
+            return selectForKey(hash(digest, 0));
+        }
+
+        private String toKey(Object[] args) {
+            StringBuilder buf = new StringBuilder();
+            for (int i : argumentIndex) {
+                if (i >= 0 && i < args.length) {
+                    buf.append(args[i]);
+                }
+            }
+            return buf.toString();
+        }
+
+        private Invoker<T> selectForKey(long hash) {
+            Map.Entry<Long, Invoker<T>> entry = virtualInvokers.ceilingEntry(hash);
+            if (entry == null) {
+                entry = virtualInvokers.firstEntry();
+            }
+            return entry.getValue();
+        }
+
+        private String byteToString(byte[] digest, int number) {
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0;i<digest.length;i++){
+               sb.append(digest);
+           }
+           return sb;
+        }
+
+        private byte[] md5(String value) {
+            MessageDigest md5;
+            try {
+                md5 = MessageDigest.getInstance("MD5");
+            } catch (NoSuchAlgorithmException e) {
+                throw new IllegalStateException(e.getMessage(), e);
+            }
+            md5.reset();
+            byte[] bytes = value.getBytes(StandardCharsets.UTF_8);
+            md5.update(bytes);
+            return md5.digest();
+        }
+
+    }
+}
