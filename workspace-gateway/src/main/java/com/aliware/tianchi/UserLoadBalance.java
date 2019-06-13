ed211c5cb846c28af10e552423cafc683ff1ccbc
chengxingyuan <1817802738@qq.com>
2019-06-13 15:42:30 +0800

    使用随机负载均衡，优先选择和ip相同的服务

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -6,6 +6,7 @@ import org.apache.dubbo.rpc.Invoker;
 import org.apache.dubbo.rpc.RpcException;
 import org.apache.dubbo.rpc.cluster.LoadBalance;
 
+import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -22,102 +23,29 @@ import java.util.concurrent.ThreadLocalRandom;
  * 此类可以修改实现，不可以移动类或者修改包名
  * 选手需要基于此类实现自己的负载均衡算法
  */
-public class UserLoadBalance implements LoadBalance {
+public class UserLoadBalance extends RandomLoadBalance {
 
     @Override
-    public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
-
-        String methodName = RpcUtils.getMethodName(invocation);
-        String key = invokers.get(0).getUrl().getServiceKey() + "." + methodName;
-        int identityHashCode = System.identityHashCode(invokers);
-        UniformityHashSelector<T> selector = (UniformityHashSelector<T>) selectors.get(key);
-        if (selector == null || selector.identityHashCode != identityHashCode) {
-            selectors.put(key, new UniformityHashSelector<T>(invokers, methodName, identityHashCode));
-            selector = (UniformityHashSelector<T>) selectors.get(key);
-        }
-        return selector.select(invocation);
+    public <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
 
 
-        return invokers.get(ThreadLocalRandom.current().nextInt(invokers.size()));
-    }
-
-
-    private static final class UniformityHashSelector<T> {
-
-        private final TreeMap<String, Invoker<T>> virtualInvokers;
-
-        private final int replicaNumber;
-
-        private final int identityHashCode;
-
-        private final int[] argumentIndex;
-
-        UniformityHashSelector(List<Invoker<T>> invokers, String methodName, int identityHashCode) {
-            this.virtualInvokers = new TreeMap<Long, Invoker<T>>();
-            this.identityHashCode = identityHashCode;
-            URL url = invokers.get(0).getUrl();
-            this.replicaNumber = url.getMethodParameter(methodName, HASH_NODES, 160);
-            String[] index = Constants.COMMA_SPLIT_PATTERN.split(url.getMethodParameter(methodName, HASH_ARGUMENTS, "0"));
-            argumentIndex = new int[index.length];
-            for (int i = 0; i < index.length; i++) {
-                argumentIndex[i] = Integer.parseInt(index[i]);
-            }
-            for (Invoker<T> invoker : invokers) {
-                String address = invoker.getUrl().getAddress();
-                for (int i = 0; i < replicaNumber / 5; i++) {
-                    byte[] digest = md5(address + i);
-                    for (int h = 0; h < 4; h++) {
-                        String m =byteToString(digest, h);
-                        virtualInvokers.put(m, invoker);
-                    }
-                }
-            }
-        }
-
-        public Invoker<T> select(Invocation invocation) {
-            String key = toKey(invocation.getArguments());
-            byte[] digest = md5(key);
-            return selectForKey(hash(digest, 0));
-        }
-
-        private String toKey(Object[] args) {
-            StringBuilder buf = new StringBuilder();
-            for (int i : argumentIndex) {
-                if (i >= 0 && i < args.length) {
-                    buf.append(args[i]);
-                }
-            }
-            return buf.toString();
-        }
-
-        private Invoker<T> selectForKey(long hash) {
-            Map.Entry<Long, Invoker<T>> entry = virtualInvokers.ceilingEntry(hash);
-            if (entry == null) {
-                entry = virtualInvokers.firstEntry();
-            }
-            return entry.getValue();
-        }
-
-        private String byteToString(byte[] digest, int number) {
-            StringBuilder sb = new StringBuilder("");
-            for (int i = 0;i<digest.length;i++){
-               sb.append(digest);
-           }
-           return sb;
-        }
-
-        private byte[] md5(String value) {
-            MessageDigest md5;
+        // 循环当前注册的多个invoker 优先选择与自己IP相同的服务
+        for (Invoker t : invokers) {
             try {
-                md5 = MessageDigest.getInstance("MD5");
-            } catch (NoSuchAlgorithmException e) {
-                throw new IllegalStateException(e.getMessage(), e);
+                InetAddress addr = InetAddress.getLocalHost();
+                String ip=addr.getHostAddress().toString(); //获取本机ip
+
+                URL u = t.getUrl();
+
+                if (u.getIp().equals(ip)) {
+                    return  t;
+                }
+            } catch (Exception e) {
             }
-            md5.reset();
-            byte[] bytes = value.getBytes(StandardCharsets.UTF_8);
-            md5.update(bytes);
-            return md5.digest();
         }
 
+        return super.doSelect(invokers, url, invocation);
+
     }
+
 }
